ba361d7bb1019a33da5d7d82975d8ac8b5b112ac
Jorge Francisco Cortés López <kokofrank@ciencias.unam.mx>
2019-08-17 19:34:21 -0500

    Documentación

--- src/filtmage.py
+++ src/filtmage.py
@@ -1,7 +1,5 @@
 import sys
-from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget, QPushButton,
- QAction, QFileDialog, QLabel, QSizePolicy, QComboBox, QSlider, QVBoxLayout,
- QLineEdit, QFrame)
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QPixmap, QImage, QPalette
 from PyQt5.QtCore import pyqtSlot, Qt
 from PyQt5 import QtCore, QtGui, QtWidgets
@@ -10,35 +8,41 @@ import filtroBlancoyNegro
 import filtroColores
 import filtroAltoContraste
 
+'''
+Clase que muestra una interfáz gráfica para el programa de
+filtros que puede abrir una imágen y modificarla.
+'''
 class App(QMainWindow):
 
+    #Atributos que tendrá la ventana.
     def __init__(self):
         super().__init__()
         self.title = 'Filtmage'
-        self.left = 100
-        self.top = 100
+        self.left = 200
+        self.top = 200
         self.width = 800
         self.height = 440
-        self.initUI()
         self.nombreArch = None
+        self.initUI()
         
         
+    #Función principal que muestra la ventana con la interfáz gráfica.   
     def initUI(self):
 
+        #Inicializamos la ventana.
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         
-        #Menu bar.
+        #Barra de Menú.
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu('Archivo')
         abrir = QAction('Abrir', self)
         fileMenu.addAction(abrir)
         abrir.setShortcut('Ctrl+O')
-
         #Para abrir archivo
         abrir.triggered.connect(self.openFileNameDialog)
 
-        #Para mostrar la imágen.
+        #Para mostrar la imágen principal sin modificar.
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
         self.imageLabel = QtWidgets.QLabel(self.centralwidget)
@@ -47,29 +51,30 @@ class App(QMainWindow):
         self.imageLabel.setText("Abre una imágen para aplicar un filtro")
         self.imageLabel.setFrameShape(QFrame.StyledPanel)
 
-        #Imágen con el filtro aplicado.
+        #Para mostrar la imágen con el filtro aplicado.
         self.imageFiltro = QtWidgets.QLabel(self.centralwidget)
         self.imageFiltro.setGeometry(QtCore.QRect(450, 100, 281, 271))
         self.imageFiltro.setScaledContents(True)
         self.imageFiltro.setFrameShape(QFrame.StyledPanel)
 
-        #Combo box para los filtros.
+        #Combo box para los filtros que puede aplicar el usuario.
         self.comboBox = QComboBox(self)
         self.comboBox.move(60,50)
         self.comboBox.addItems(["Blanco Y Negro", "Rojo", "Verde", "Azul",
             "Mosaico", "Alto Contraste", "Inverso", "Brillo"])
 
-        #Botón aplica filtro.
+        #Botón para aplicar un filtro a la imágen abierta.
         self.aplicaFiltro = QPushButton("Aplica filtro", self)
         self.aplicaFiltro.move(240, 50)
         self.aplicaFiltro.clicked.connect(self.filtro)
 
-        #Slider para algunos filtros
+        #Caja que contiene la información del slider.
         vBoxLayout = QVBoxLayout()
         self.lineEdit = QLineEdit(self)
         vBoxLayout.addWidget(self.lineEdit)
         self.lineEdit.move(620, 50)
 
+        #Slider para los filtros de brillo y mosaico.
         self.sliderFiltro = QSlider(Qt.Horizontal, self)
         self.sliderFiltro.move(450, 50)
         self.sliderFiltro.setMinimum(-127)
@@ -81,13 +86,14 @@ class App(QMainWindow):
         self.setCentralWidget(self.centralwidget)
         self.show()
 
-    #Aplica el filtro seleccionado a la imágen.
+    #Aplica el filtro seleccionado a la imágen y la muestra en la segunda caja.
     def filtro(self):
         filtro = self.comboBox.currentText()
         valor = int(self.sliderFiltro.value())
+
         #Seleccionamos el filtro adecuado.
         if (filtro == "Blanco Y Negro"):
-            imagen = filtroBlancoYNegro.blancoYNegro(self.nombreArch)
+            imagen = filtroBlancoyNegro.blancoYNegro(self.nombreArch)
         if (filtro == "Rojo"):
             imagen = filtroColores.filtroRojo(self.nombreArch)
         if (filtro == "Verde"):
@@ -108,8 +114,6 @@ class App(QMainWindow):
         qim = QtGui.QImage(data, imagen.size[0], imagen.size[1], QtGui.QImage.Format_ARGB32)
         pix = QtGui.QPixmap.fromImage(qim)
         self.imageFiltro.setPixmap(pix)
-        #filtro = self.comboBox.currentText()
-
 
     #File Dialog para abrir un archivo de imágen.
     def openFileNameDialog(self):
@@ -124,8 +128,10 @@ class App(QMainWindow):
     def changedValue(self):
         size = str(self.sliderFiltro.value())
         self.lineEdit.setText(size)
-    
+
+#Inicializa la interfáz gráfica.
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
+    
